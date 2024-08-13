89e0b98822f5c37811167690d4e23071d86db29c
Ahmed Hussein <wolktheline@yahoo.com>
2024-08-12 22:51:20 -0600

    Adding a column between two columns for space.

--- main.py
+++ main.py
@@ -22,19 +22,24 @@ Below you can find anything you want, feel free to contact me! And I'm adding ex
 
 st.write(content2)
 
-col3, col4 = st.columns(2)
+col3, col4, col5 = st.columns([1.5, 0.5, 1.5])
+
 
 df = pd.read_csv("data.csv", sep=";")
 
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
-        st.image(f"images/{row['image']}")
         st.write(row["description"])
+        st.image(f"images/{row['image']}")
+        st.write(f"[Source Code]({row['url']})")
 
 with col4:
-     for index, row in df[10:].iterrows():
-        st.header(row["title"])
-        st.image(f"images/{row['image']}")
-        st.write(row["description"])
+    pass
 
+with col5:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image(f"images/{row['image']}")
+        st.write(f"[Source Code]({row['url']})")
