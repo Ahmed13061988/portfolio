693ff24e8e8f0e129f041832f9ebce4ba524e411
Ahmed Hussein <wolktheline@yahoo.com>
2024-08-13 20:52:45 -0600

    Sending email script

--- pages/Contact_Me.py
+++ pages/Contact_Me.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
-st.header("Contact Me")
+
+with st.form(key="email_form"):
+    users_email = st.text_input("Your email address")
+    email_body = st.text_area("Your message")
+    button = st.form_submit_button("Submit")
+    if button:
+        message = email_body + users_email
+
